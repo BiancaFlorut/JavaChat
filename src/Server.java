d16604cb7e5f519fa9bde8f242af51d3de3d1342
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-07-29 14:18:52 +0200

    server class

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,31 @@
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class Server {
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Enter the server port number: ");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Server is working on port: " + port);
+            while (true) {
+                Socket client = serverSocket.accept();
+                Handler handler = new Handler(client);
+                handler.start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("An error has occurred. " + e.getMessage());
+        }
     }
 }
