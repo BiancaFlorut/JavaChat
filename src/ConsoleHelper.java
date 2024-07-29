3b9adaf9a7675ce335306cd56f3c929fe8bc57f5
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-07-29 11:48:22 +0200

    first commit

---
+++ src/ConsoleHelper.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String line = null;
+        while (true) {
+            try {
+                line = reader.readLine();
+                if (line != null) return line;
+            } catch (IOException e) {
+                writeMessage("An error occurred while trying to enter text. Try again.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                String line = readString();
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                writeMessage("An error occurred while trying to enter a number. Try again.");
+            }
+        }
+    }
+}
