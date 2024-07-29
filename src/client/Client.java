1b0e5d6100b802cac4f35d82610113de589c21e5
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-07-29 17:13:05 +0200

    client protocol, test

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,10 +6,75 @@ import main.Message;
 import main.MessageType;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
     public class SocketThread extends Thread {
 
+        public void run() {
+            String serverAddress = getServerAddress();
+            int portNumber = getServerPort();
+            try (Socket socket = new Socket(serverAddress, portNumber)){
+                connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("An error occurred while opening the socket connection.");
+                notifyConnectionStatusChanged(false);
+            } catch (ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("An error occurred in the client connection protocol.");
+                notifyConnectionStatusChanged(false);
+            }
+
+        }
+
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUse(String userName) {
+            ConsoleHelper.writeMessage(userName + "has joined the chat.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage(userName + " has left the Chat.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.NAME_REQUEST) {
+                    String userName = getUserName();
+                    connection.send(new Message(MessageType.USER_NAME, userName));
+                } else
+                if (message.getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    break;
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+                } else if(message.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUse(message.getData());
+                } else if (message.getType() == MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(message.getData());
+                } else throw new IOException("Unexpected MessageType");
+            }
+        }
     }
     protected Connection connection;
     private volatile boolean clientConnected = false;
@@ -61,7 +126,7 @@ public class Client {
         }
         if (clientConnected) {
             ConsoleHelper.writeMessage("Connection established. To exit, enter 'exit'.");
-        } ConsoleHelper.writeMessage("An error occurred while working with the client.");
+        } else ConsoleHelper.writeMessage("An error occurred while working with the client.");
         while (clientConnected) {
             String line = ConsoleHelper.readString();
             if (line.equals("exit")) break;
